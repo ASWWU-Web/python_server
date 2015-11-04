81e44a398e59dba7e2fd0a7e403d4084b9a680da
Brock Haugen <brock.a.haugen@gmail.com>
2015-11-03 18:54:37 -0800

    CORS fix

--- alchemy/base_handlers.py
+++ alchemy/base_handlers.py
@@ -33,6 +33,9 @@ class LoggedInUser:
 
 
 class BaseHandler(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        
     def get_current_user(self):
         token = self.request.headers.get('token', None)
         if not token:
