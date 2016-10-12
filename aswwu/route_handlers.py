43d96f6159d49427a4cfb455b78e629b318d735b
asteroidice <ryan.s.rabello@gmail.com>
2016-10-11 19:52:52 -0700

    added elections are now closed.

--- aswwu/route_handlers.py
+++ aswwu/route_handlers.py
@@ -478,27 +478,28 @@ class AllElectionVoteHandler(BaseHandler):
 class ElectionVoteHandler(BaseHandler):
     @tornado.web.authenticated
     def post(self, username):
-        user = self.current_user
-        if user.username == username or 'administrator' in user.roles:
-            usrvote = query_by_wwuid_Election(Election, str(user.wwuid))
-            # Fix this to be more efficient
-            if len(usrvote) == 0:
-                new_vote = Election(wwuid=str(user.wwuid))
-                vote = addOrUpdateElection(new_vote)
-            else:
-                vote = election_s.query(Election).filter_by(wwuid=str(user.wwuid)).one()
-            vote.candidate_one = self.get_argument('candidate_one','')
-            vote.candidate_two = self.get_argument('candidate_two','')
-            vote.sm_one = self.get_argument('sm_one','')
-            vote.sm_two = self.get_argument('sm_two','')
-            vote.new_department = self.get_argument('new_department','')
-            vote.district = self.get_argument('district', '')
-
-            addOrUpdateElection(vote)
-
-            self.write({'vote': 'successfully voted'})
-        else:
-            self.write({'error': 'invalid voting permissions'})
+        self.write({"error": "Elections are now closed."})
+        # user = self.current_user
+        # if user.username == username or 'administrator' in user.roles:
+        #     usrvote = query_by_wwuid_Election(Election, str(user.wwuid))
+        #     # Fix this to be more efficient
+        #     if len(usrvote) == 0:
+        #         new_vote = Election(wwuid=str(user.wwuid))
+        #         vote = addOrUpdateElection(new_vote)
+        #     else:
+        #         vote = election_s.query(Election).filter_by(wwuid=str(user.wwuid)).one()
+        #     vote.candidate_one = self.get_argument('candidate_one','')
+        #     vote.candidate_two = self.get_argument('candidate_two','')
+        #     vote.sm_one = self.get_argument('sm_one','')
+        #     vote.sm_two = self.get_argument('sm_two','')
+        #     vote.new_department = self.get_argument('new_department','')
+        #     vote.district = self.get_argument('district', '')
+        #
+        #     addOrUpdateElection(vote)
+        #
+        #     self.write({'vote': 'successfully voted'})
+        # else:
+        #     self.write({'error': 'invalid voting permissions'})
 
 class ElectionLiveFeedHandler(BaseHandler):
     def get(self):
