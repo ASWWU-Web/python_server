7273ce492aca5f84a53d74ba1d2e1287550c1b6a
Ethan Beaver <ethan.beaver@ahss.org>
2018-02-07 17:55:41 -0800

    Adding page creation functionality

--- server.py
+++ server.py
@@ -74,7 +74,7 @@ class Application(tornado.web.Application):
             (r"/askanything/(.*)/authorize", ask_anything.AskAnythingAuthorizeHandler),
             (r"/pages", pages.GetAllHandler),
             (r"/pages/search", pages.SearchHandler),
-            # (r"/pages/admin", pages.AdminAllHandler),
+            (r"/pages/admin", pages.AdminAllHandler),
             # (r"/pages/admin/(.*)/revision", pages.GetAllRevisionsHandler),
             # (r"/pages/admin/(.*)/revision/(.*)", pages.SpecificRevisionHandler),
             # (r"/pages/admin/(.*)", pages.AdminSpecificPageHandler),
