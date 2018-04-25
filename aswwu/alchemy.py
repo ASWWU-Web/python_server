2d06c896984a757a13c49bcec33d4dddd043aa67
ethanbeaver <ethanbeaver35@gmail.com>
2018-04-25 14:14:14 -0700

    CMS Integration (#60)
    * Fixing some Pages stuff
    
    * Adding comment for future target
    
    * Preliminary setup of route urls and models
    
    * Adding function for getting all pages from database
    
    * Updated serialize function to properly use the Pages model.
    
    * Styling fix.
    
    * Updated pages model with better names and proper variable.
    
    * Added GetAllHandler.
    
    * Added GetHandler and created a minified serialize function for GetAllHandler.
    
    * Cleanup. Returns HTTP Status code 500 on exceptions.
    
    * Added description field to Page model.
    
    * Renamed logger errors and changed the exception return JSON.
    
    * Feature/cms sheldon (#45)
    
    * Added GetAllHandler.
    
    * Added GetHandler and created a minified serialize function for GetAllHandler.
    
    * Cleanup. Returns HTTP Status code 500 on exceptions.
    
    * Added description field to Page model.
    
    * Renamed logger errors and changed the exception return JSON.
    
    * Adding search handler
    
    * Adding page creation functionality
    
    * Added AdminAllHandler, get_editor_pages(), and get_owner_pages.
    
    * Combined GET and POST method for AdminAllHandler.
    
    * changed error to status for no page found on GetHandler.
    
    * Added AdminSpecificPageHandler.
    
    * Minor serialization changes
    
    * Combined alchemy functions and cleaned up Admin handlers.
    
    * Removing unneeded function
    
    * Rewrote AdminSpecificPageHandler.
    
    * Removed unused get_editor_page() fucntion.
    
    * Adding update functionality
    
    * Adding creation of revisions
    
    * Updated AdminSpecificPageHandler GET route and AdminAllHandler.
    
    * Added AdminSpecificPageHandler DELETE route. Needs some functionality from Ethan's branch.
    
    * Added CategoryHandler and DepartmentHandler.
    
    * AdminSpecificPageHandler DELETE route now working completely.
    
    * serializers now return tag and editor lists in CSV format.
    
    * Removed CSV formatting from GET requests. POST requests now take JSON for tags and editors fields.
    
    * Fixing current attribute of page in edit page
    
    * Pages are no longer deleted but just set to not current.
    
    * Added handler for getting all tags.
    
    * Converting from form-data to application/json requests
    
    * Removing print statements
    
    * Checks for editors. Checks for some column updates based on permissions.
    
    * Introduction revision functionality
    
    * All columns checked for proper owner/editor permissions.
    
    * Updated Page model with author.
    
    * Adding response to revision restore endpoint
    
    * Cleanup. New revisions inherit old values.
    
    * Added checking for bad categories and departments.
    
    * Final cleanup for AdminSpecificPageHandler POST route.
    
    * Fixed HTTP 401/403 inconsistencies.
    
    * Added FeaturedsHandler.
    
    * Pluralized departments and categories route urls.
    
    * Featured pages now unfeatured upon deletion.
    
    * Get all features fixed.
    
    * Create AdminFeaturedHandler. Added delete route to AdminFeaturedHandler.
    
    * Featured rows now always deleted on un-featuring.
    
    * Fixed bug in GET route of FeaturedsHandler.
    
    * fixed error handling for non-unique page urls
    
    * Adding routes for froala image upload and retrieval
    
    * More featured updates.
    
    * Fixing thumbnail url, returning more info after upload
    
    * Cleanup of file
    
    * Cleaning up last merge
    
    * Fixing search to show only current and visible pages
    
    * Adding cover image field to pages
    
    * Fixing issue with datetime updating
    
    * Adding extra field to page object

--- aswwu/alchemy.py
+++ aswwu/alchemy.py
@@ -1,14 +1,16 @@
 # alchemy.py
 
 # import and set up the logging
+import ast
 import logging
 
 from sqlalchemy import create_engine, func, or_, and_, desc
-from sqlalchemy.orm import sessionmaker, joinedload
+from sqlalchemy.orm import sessionmaker, joinedload, class_mapper
 from sqlalchemy.sql import label
 
 import aswwu.models.bases as base
 import aswwu.models.mask as mask_model
+import aswwu.models.pages as pages_model
 from aswwu.archive_models import ArchiveBase
 
 Base = base.Base
@@ -17,8 +19,6 @@ PagesBase = base.PagesBase
 JobsBase = base.JobsBase
 
 logger = logging.getLogger("aswwu")
-
-# import the necessary models (all of them in this case)
 
 # defines the databases URLs relative to "server.py"
 engine = create_engine("sqlite:///../databases/people.db")
@@ -46,10 +46,11 @@ ElectionBase.metadata.bind = election_engine
 election_dbs = sessionmaker(bind=election_engine)
 election_db = election_dbs()
 # same for pages
-PagesBase.metadata.bind = election_engine
+PagesBase.metadata.bind = pages_engine
 pages_dbs = sessionmaker(bind=pages_engine)
 page_db = pages_dbs()
 
+# TODO: Figure out the consequences of this mistake
 JobsBase.metadata.bind = election_engine
 jobs_dbs = sessionmaker(bind=jobs_engine)
 jobs_db = jobs_dbs()
@@ -223,37 +224,6 @@ def query_by_wwuid_election(model, wwuid):
 
 
 # updates a model, or creates it if it doesn't exist
-def add_or_update_page(thing):
-    try:
-        page_db.add(thing)
-        page_db.commit()
-        return thing
-    except Exception as e:
-        logger.info(e)
-        page_db.rollback()
-
-
-def query_by_page_url(model, url):
-    thing = None
-    try:
-        thing = page_db.query(model).options(joinedload('*')).filter_by(url=str(url)).all()
-    except Exception as e:
-        logger.info(e)
-        page_db.rollback()
-    return thing
-
-
-def query_by_page_id(model, page_id):
-    thing = None
-    try:
-        thing = page_db.query(model).options(joinedload('*')).filter_by(id=str(page_id)).all()
-    except Exception as e:
-        logger.info(e)
-        page_db.rollback()
-    return thing
-
-
-# updates a model, or creates it if it doesn't exist
 def add_or_update_form(thing):
     try:
         jobs_db.add(thing)
@@ -292,3 +262,294 @@ def delete_thing_forms(thing):
     except Exception as e:
         logger.info(e)
         jobs_db.rollback()
+
+
+# Section for ASWWU Pages Functions
+
+# updates a model, or creates it if it doesn't exist
+def add_or_update_page(thing):
+    try:
+        page_db.add(thing)
+        page_db.commit()
+        return thing
+    except Exception as e:
+        logger.info(e)
+        page_db.rollback()
+        raise Exception(e)
+
+
+def delete_thing_pages(thing):
+    try:
+        page_db.delete(thing)
+        page_db.commit()
+    except Exception as e:
+        logger.info(e)
+        jobs_db.rollback()
+
+
+def query_by_page_url(url):
+    thing = None
+    try:
+        thing = page_db.query(pages_model.Page).options(joinedload('*'))\
+            .filter_by(url=str(url), is_visible=True, current=True).one()
+    except Exception as e:
+        logger.info(e)
+        page_db.rollback()
+    return thing
+
+
+def admin_query_by_page_url(url):
+    thing = None
+    try:
+        thing = page_db.query(pages_model.Page).options(joinedload('*'))\
+            .filter_by(url=str(url), current=True).one()
+    except Exception as e:
+        logger.info(e)
+        page_db.rollback()
+    return thing
+
+
+def get_all_pages():
+    thing = None
+    try:
+        thing = page_db.query(pages_model.Page).all()
+    except Exception as e:
+        logger.info(e)
+        page_db.rollback()
+    return thing
+
+
+def pages_general_search_term_generator(search_criteria):
+    """Search for a general search term in all fields"""
+    # Search in all fields(except tags and editors)
+    for prop in class_mapper(pages_model.Page).iterate_properties:
+        if str(prop.key) != "editors" and str(prop.key) != "tags":
+            yield getattr(pages_model.Page, prop.key)\
+                .ilike("%" + search_criteria["general"] + "%")
+    # Search all tags
+    for x in pages_specific_search_term_generator(
+            {"tags": "[\"" + search_criteria["general"] + "\"]"}):
+        yield x
+
+
+def pages_specific_search_term_generator(search_criteria):
+    """Search by specific field."""
+    for key in search_criteria:
+        if hasattr(pages_model.Page, key):
+            if key == "tags":
+                search_tags = ast.literal_eval(search_criteria[key])
+                for tag in search_tags:
+                    page_tags = page_db.query(pages_model.PageTag)\
+                        .filter_by(tag=tag).all()
+                    for db_tag in page_tags:
+                        yield getattr(pages_model.Page, "url").\
+                            ilike(db_tag.url)
+            else:
+                yield getattr(pages_model.Page, key).\
+                    ilike("%" + search_criteria[key] + "%")
+    if not (len(search_criteria) == 1 and "tags" in search_criteria):
+        yield getattr(pages_model.Page, "current").ilike(1)
+        yield getattr(pages_model.Page, "is_visible").ilike(1)
+
+
+def search_pages(search_criteria):
+    """General search function for ASWWU pages."""
+    thing = None
+    try:
+        if len(search_criteria) == 1 and "general" in search_criteria:
+            search_statement = or_(
+                pages_general_search_term_generator(search_criteria)
+            )
+            search_statement = and_(
+                search_statement,
+                getattr(pages_model.Page, "current").ilike(1),
+                getattr(pages_model.Page, "is_visible").ilike(1)
+            )
+        else:
+            search_statement = and_(
+                pages_specific_search_term_generator(search_criteria)
+            )
+        thing = page_db.query(pages_model.Page). \
+            filter(search_statement)
+    except Exception as e:
+        logger.info(e)
+        page_db.rollback()
+    return thing
+
+
+def get_all_visible_current_pages():
+    thing = None
+    try:
+        thing = page_db.query(pages_model.Page).options(joinedload('*'))\
+            .filter_by(is_visible=True, current=True).all()
+    except Exception as e:
+        logger.info(e)
+        page_db.rollback()
+    return thing
+
+
+def get_admin_pages(user):
+    thing = None
+    try:
+        owned_pages = page_db.query(pages_model.Page).options(joinedload('*'))\
+            .filter_by(owner=user, current=True).all()
+        editables = page_db.query(pages_model.PageEditor)\
+            .options(joinedload('*')).filter_by(username=user).all()
+        editable_pages = []
+        for editable in editables:
+            editable_pages.append(admin_query_by_page_url(editable.url))
+        thing = owned_pages + editable_pages
+    except Exception as e:
+        logger.info(e)
+        page_db.rollback()
+    return thing
+
+
+def get_editors(url):
+    thing = None
+    try:
+        thing = page_db.query(pages_model.PageEditor).options(joinedload('*'))\
+            .filter_by(url=str(url)).all()
+    except Exception as e:
+        logger.info(e)
+        page_db.rollback()
+    return thing
+
+
+def query_page_tags(url):
+    thing = None
+    try:
+        thing = page_db.query(pages_model.PageTag).options(joinedload('*'))\
+            .filter_by(url=str(url)).all()
+    except Exception as e:
+        logger.info(e)
+        page_db.rollback()
+    return thing
+
+
+def get_all_tags():
+    thing = None
+    try:
+        thing = page_db.query(pages_model.PageTag).options(joinedload('*')).all()
+    except Exception as e:
+        logger.info(e)
+        page_db.rollback()
+    return thing
+
+
+def get_tags(url):
+    thing = None
+    try:
+        thing = page_db.query(pages_model.PageTag).options(joinedload('*'))\
+            .filter_by(url=str(url)).all()
+    except Exception as e:
+        logger.info(e)
+        page_db.rollback()
+    return thing
+
+
+def query_page_tag(url, tag):
+    thing = None
+    try:
+        thing = page_db.query(pages_model.PageTag).options(joinedload('*'))\
+            .filter_by(url=str(url), tag=tag).one()
+    except Exception as e:
+        logger.info(e)
+        page_db.rollback()
+    return thing
+
+
+def get_all_featureds():
+    thing = None
+    try:
+        thing = page_db.query(pages_model.Featured).options(joinedload('*')).all()
+    except Exception as e:
+        logger.info(e)
+        page_db.rollback()
+    return thing
+
+
+def get_featured(url):
+    thing = None
+    try:
+        thing = page_db.query(pages_model.Featured).options(joinedload('*'))\
+            .filter_by(url=str(url)).one()
+    except Exception as e:
+        logger.info(e)
+        page_db.rollback()
+    return thing
+
+
+def query_page_editors(url):
+    thing = None
+    try:
+        thing = page_db.query(pages_model.PageEditor).options(joinedload('*'))\
+            .filter_by(url=str(url)).all()
+    except Exception as e:
+        logger.info(e)
+        page_db.rollback()
+    return thing
+
+
+def get_categories():
+    thing = None
+    try:
+        thing = page_db.query(pages_model.Category).options(joinedload('*')).all()
+    except Exception as e:
+        logger.info(e)
+        page_db.rollback()
+    return thing
+
+
+def query_page_editor(url, username):
+    thing = None
+    try:
+        thing = page_db.query(pages_model.PageEditor).options(joinedload('*'))\
+            .filter_by(url=str(url), username=username).one()
+    except Exception as e:
+        logger.info(e)
+        page_db.rollback()
+    return thing
+
+
+def get_departments():
+    thing = None
+    try:
+        thing = page_db.query(pages_model.Department).options(joinedload('*')).all()
+    except Exception as e:
+        logger.info(e)
+        page_db.rollback()
+    return thing
+
+
+def get_all_page_revisions(url):
+    thing = None
+    try:
+        thing = page_db.query(pages_model.Page).options(joinedload('*'))\
+            .filter_by(url=url).all()
+    except Exception as e:
+        logger.info(e)
+        page_db.rollback()
+    return thing
+
+
+def get_specific_page_revision(url, revision_id):
+    thing = None
+    try:
+        thing = page_db.query(pages_model.Page).options(joinedload('*'))\
+            .filter_by(id=revision_id, url=url).one()
+    except Exception as e:
+        logger.info(e)
+        page_db.rollback()
+    return thing
+
+
+# TODO: duplicate function
+# permanently deletes a given model
+def delete_pages_thing(thing):
+    try:
+        page_db.delete(thing)
+        page_db.commit()
+    except Exception as e:
+        logger.info(e)
+        page_db.rollback()
