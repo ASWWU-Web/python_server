08a8ab45c8d29e52e4accb2ee94b1ee13ea612d0
morini <nicholas.morin@wallawalla.edu>
2018-10-09 13:48:08 -0700

    moved jobs from alchemy.py to new file under alchemy_new

--- aswwu/alchemy.py
+++ aswwu/alchemy.py
@@ -193,45 +193,7 @@ def delete_thing(thing):
         people_db.rollback()
 
 
-# updates a model, or creates it if it doesn't exist
-def add_or_update_form(thing):
-    try:
-        jobs_db.add(thing)
-        jobs_db.commit()
-        return thing
-    except Exception as e:
-        logger.info(e)
-        jobs_db.rollback()
 
-
-def query_by_job_name(model, name):
-    thing = None
-    try:
-        thing = jobs_db.query(model).options(joinedload('*')).filter_by(job_name=str(name)).all()
-    except Exception as e:
-        logger.info(e)
-        jobs_db.rollback()
-    return thing
-
-
-def query_all_forms(model):
-    thing = None
-    try:
-        thing = jobs_db.query(model).all()
-    except Exception as e:
-        logger.info(e)
-        jobs_db.rollback()
-    return thing
-
-
-# permanently deletes a given model
-def delete_thing_forms(thing):
-    try:
-        jobs_db.delete(thing)
-        jobs_db.commit()
-    except Exception as e:
-        logger.info(e)
-        jobs_db.rollback()
 
 
 # Section for ASWWU Pages Functions
---
+++ aswwu/alchemy_new/jobs.py
@@ -0,0 +1,65 @@
+# alchemy.py
+
+# import and set up the logging
+import ast
+import logging
+
+from sqlalchemy import create_engine, func, or_, and_, desc
+from sqlalchemy.orm import sessionmaker, joinedload, class_mapper
+from sqlalchemy.sql import label
+
+import aswwu.models.bases as base
+
+JobsBase = base.JobsBase
+
+logger = logging.getLogger("aswwu")
+
+jobs_engine = create_engine("sqlite:///../databases/jobs.db")
+election_engine = create_engine("sqlite:///../databases/senate_elections.db")
+
+JobsBase.metadata.create_all(jobs_engine)
+
+# TODO: Figure out the consequences of this mistake
+JobsBase.metadata.bind = election_engine
+jobs_dbs = sessionmaker(bind=jobs_engine)
+jobs_db = jobs_dbs()
+
+# updates a model, or creates it if it doesn't exist
+def add_or_update_form(thing):
+    try:
+        jobs_db.add(thing)
+        jobs_db.commit()
+        return thing
+    except Exception as e:
+        logger.info(e)
+        jobs_db.rollback()
+
+
+def query_by_job_name(model, name):
+    thing = None
+    try:
+        thing = jobs_db.query(model).options(joinedload('*')).filter_by(job_name=str(name)).all()
+    except Exception as e:
+        logger.info(e)
+        jobs_db.rollback()
+    return thing
+
+
+def query_all_forms(model):
+    thing = None
+    try:
+        thing = jobs_db.query(model).all()
+    except Exception as e:
+        logger.info(e)
+        jobs_db.rollback()
+    return thing
+
+
+# permanently deletes a given model
+def delete_thing_forms(thing):
+    try:
+        jobs_db.delete(thing)
+        jobs_db.commit()
+    except Exception as e:
+        logger.info(e)
+        jobs_db.rollback()
