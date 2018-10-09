d425a17dae49ceb2d5bd38359170b4b726d7cf67
morini <nicholas.morin@wallawalla.edu>
2018-10-09 14:06:09 -0700

    moved archive funtions from alchemy to alchemy_new folder

---
+++ aswwu/alchemy_new/archive.py
@@ -0,0 +1,22 @@
+# alchemy.py
+
+# import and set up the logging
+import ast
+import logging
+
+from sqlalchemy import create_engine, func, or_, and_, desc
+from sqlalchemy.orm import sessionmaker, joinedload, class_mapper
+from sqlalchemy.sql import label
+
+from aswwu.archive_models import ArchiveBase
+
+logger = logging.getLogger("aswwu")
+
+# defines the databases URLs relative to "server.py"
+archive_engine = create_engine("sqlite:///../databases/archives.db")
+
+# same for archives
+ArchiveBase.metadata.bind = archive_engine
+archive_dbs = sessionmaker(bind=archive_engine)
+archive_db = archive_dbs()
+
