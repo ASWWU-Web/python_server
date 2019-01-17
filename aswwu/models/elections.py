09888ed8a559068733426774cbc2bf6a3451b064
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2019-01-16 18:46:01 -0800

    Moved model validations to elections/models.py.

--- aswwu/models/elections.py
+++ aswwu/models/elections.py
@@ -1,6 +1,10 @@
 from sqlalchemy import Column, ForeignKey, String, DateTime, Integer, Boolean
+from datetime import datetime
 
 from aswwu.models.bases import ElectionBase
+import aswwu.alchemy_new.elections as elections_alchemy
+import aswwu.alchemy_new.mask as mask_alchemy
+import aswwu.exceptions as exceptions
 
 
 class Election(ElectionBase):
@@ -16,6 +20,18 @@ class Election(ElectionBase):
             'end': str(self.end),
         }
 
+    @staticmethod
+    def validate(parameters):
+        # check that election doesn't overlap with current or upcoming elections
+        if elections_alchemy.detect_election_overlap(parameters["start"], parameters["end"]):
+            raise exceptions.Forbidden403Exception('election takes place during another election')
+        # checking that election doesn't start time in the past
+        if elections_alchemy.detect_election_start(parameters["start"], parameters["end"]):
+            raise exceptions.Forbidden403Exception('election takes place during the past')
+        # check that end time isn't less than start time
+        if elections_alchemy.detect_bad_end(parameters["start"], parameters["end"]):
+            raise exceptions.Forbidden403Exception('start time is after end time')
+
 
 class Position(ElectionBase):
     position = Column(String(100))
@@ -29,6 +45,11 @@ class Position(ElectionBase):
             'election_type': self.election_type,
             'active': self.active,
         }
+
+    @staticmethod
+    def validate(parameters):
+        if not isinstance(parameters['active'], bool):
+            raise exceptions.BadRequest400Exception('parameter active has type bool')
 
 
 class Vote(ElectionBase):
@@ -46,6 +67,52 @@ class Vote(ElectionBase):
             'username': self.username,
         }
 
+    @staticmethod
+    def validate(parameters, existing_vote=None):
+        # check if election exists
+        specified_election = elections_alchemy.query_election(election_id=parameters['election'])
+        if specified_election == list():
+            raise exceptions.NotFound404Exception('election with specified ID not found')
+
+        # check if not current election
+        current_election = elections_alchemy.query_current()
+        if specified_election[0] != current_election:
+            raise exceptions.Forbidden403Exception('this election is not available for voting')
+
+        # check if position exists and is active
+        specified_position = elections_alchemy.query_position(position_id=parameters['position'])
+        if specified_position == list() or specified_position[0].active is False:
+            raise exceptions.Forbidden403Exception('position with specified ID not found')
+
+        # check if position is the right election type
+        if specified_position[0].election_type != current_election.election_type:
+            raise exceptions.Forbidden403Exception('you are voting for a position in a different election type')
+
+        # check for valid candidate username
+        if mask_alchemy.query_by_username(parameters['vote']) is None:
+            raise exceptions.Forbidden403Exception('you cannot vote for this person')
+
+        # check amount of votes a user has in aswwu or senate election
+        if existing_vote is None:
+            if specified_election[0].election_type == 'aswwu' and \
+                    len(elections_alchemy.query_vote(election=specified_election[0].id,
+                                                     position=specified_position[0].id,
+                                                     username=parameters['username'])) >= 1:
+                raise exceptions.Forbidden403Exception('you can only vote for one aswwu representative')
+            elif specified_election[0].election_type == 'senate' and \
+                    len(elections_alchemy.query_vote(election=specified_election[0].id,
+                                                     position=specified_position[0].id,
+                                                     username=parameters['username'])) >= 2:
+                raise exceptions.Forbidden403Exception('you can only vote for two senators')
+
+        # check for duplicate votes
+        if parameters['vote'] != getattr(existing_vote, 'vote', None) and \
+                elections_alchemy.query_vote(election=specified_election[0].id,
+                                             position=specified_position[0].id,
+                                             vote=parameters['vote'],
+                                             username=parameters['username']) != list():
+            raise exceptions.Forbidden403Exception('you have already voted for this person')
+
 
 class Candidate(ElectionBase):
     election = Column(String(100), ForeignKey('elections.id'))
@@ -61,3 +128,24 @@ class Candidate(ElectionBase):
             'username': self.username,
             'display_name': self.display_name,
         }
+
+    @staticmethod
+    def validate(parameters):
+        # check to make sure there is an election to push candidates to
+        election = elections_alchemy.query_election(election_id=parameters['election'])
+        if election == list():
+            raise exceptions.NotFound404Exception('election with specified ID not found')
+        election = election[0]
+
+        # check to make sure election is either current or up and coming
+        if election != elections_alchemy.query_current() and \
+                election not in elections_alchemy.query_election(start=datetime.now()):
+            raise exceptions.Forbidden403Exception('candidate not in current election')
+
+        # check to makes sure position exists
+        if not elections_alchemy.query_position(position_id=str(parameters["position"])):
+            raise exceptions.NotFound404Exception('position with specified ID not found')
+
+        # check to make sure election exists
+        if not elections_alchemy.query_election(election_id=parameters['election']):
+            raise exceptions.NotFound404Exception('election with specified ID not found')
--- aswwu/route_handlers/elections.py
+++ aswwu/route_handlers/elections.py
@@ -7,7 +7,6 @@ from datetime import datetime
 
 from aswwu.base_handlers import BaseHandler
 import aswwu.alchemy_new.elections as elections_alchemy
-import aswwu.alchemy_new.mask as mask_alchemy
 import aswwu.models.elections as elections_model
 import aswwu.exceptions as exceptions
 
@@ -28,90 +27,6 @@ def validate_parameters(given_parameters, required_parameters):
     # check for bad election type
     if 'election_type' in given_parameters.keys() and given_parameters['election_type'] not in ('aswwu', 'senate'):
         raise exceptions.BadRequest400Exception('election_type is not aswwu or senate')
-
-
-def validate_vote(user, parameters, existing_vote=None):
-    # check if election exists
-    specified_election = elections_alchemy.query_election(election_id=parameters['election'])
-    if specified_election == list():
-        raise exceptions.NotFound404Exception('election with specified ID not found')
-
-    # check if not current election
-    current_election = elections_alchemy.query_current()
-    if specified_election[0] != current_election:
-        raise exceptions.Forbidden403Exception('this election is not available for voting')
-
-    # check if position exists and is active
-    specified_position = elections_alchemy.query_position(position_id=parameters['position'])
-    if specified_position == list() or specified_position[0].active is False:
-        raise exceptions.Forbidden403Exception('position with specified ID not found')
-
-    # check if position is the right election type
-    if specified_position[0].election_type != current_election.election_type:
-        raise exceptions.Forbidden403Exception('you are voting for a position in a different election type')
-
-    # check for valid candidate username
-    if mask_alchemy.query_by_username(parameters['vote']) is None:
-        raise exceptions.Forbidden403Exception('you cannot vote for this person')
-
-    # check amount of votes a user has in aswwu or senate election
-    if existing_vote is None:
-        if specified_election[0].election_type == 'aswwu' and \
-                len(elections_alchemy.query_vote(election=specified_election[0].id,
-                                                 position=specified_position[0].id,
-                                                 username=str(user.username))) >= 1:
-            raise exceptions.Forbidden403Exception('you can only vote for one aswwu representative')
-        elif specified_election[0].election_type == 'senate' and \
-                len(elections_alchemy.query_vote(election=specified_election[0].id,
-                                                 position=specified_position[0].id,
-                                                 username=str(user.username))) >= 2:
-            raise exceptions.Forbidden403Exception('you can only vote for two senators')
-
-    # check for duplicate votes
-    if parameters['vote'] != getattr(existing_vote, 'vote', None) and \
-            elections_alchemy.query_vote(election=specified_election[0].id,
-                                         position=specified_position[0].id,
-                                         vote=parameters['vote'],
-                                         username=str(user.username)) != list():
-        raise exceptions.Forbidden403Exception('you have already voted for this person')
-
-
-def validate_election(parameters):
-    # check that election doesn't overlap with current or upcoming elections
-    if elections_alchemy.detect_election_overlap(parameters["start"], parameters["end"]):
-        raise exceptions.Forbidden403Exception('election takes place during another election')
-    # checking that election doesn't start time in the past
-    if elections_alchemy.detect_election_start(parameters["start"], parameters["end"]):
-        raise exceptions.Forbidden403Exception('election takes place during the past')
-    # check that end time isn't less than start time
-    if elections_alchemy.detect_bad_end(parameters["start"], parameters["end"]):
-        raise exceptions.Forbidden403Exception('start time is after end time')
-
-
-def validate_position(parameters):
-    if not isinstance(parameters['active'], bool):
-        raise exceptions.BadRequest400Exception('parameter active has type bool')
-
-
-def validate_candidate(parameters, election_id):
-    # check to make sure there is an election to push candidates to
-    election = elections_alchemy.query_election(election_id=str(election_id))
-    if election == list():
-        raise exceptions.NotFound404Exception('election with specified ID not found')
-    election = election[0]
-
-    # check to make sure election is either current or up and coming
-    if election != elections_alchemy.query_current() and \
-            election not in elections_alchemy.query_election(start=datetime.now()):
-        raise exceptions.Forbidden403Exception('candidate not in current election')
-
-    # check to makes sure position exists
-    if not elections_alchemy.query_position(position_id=str(parameters["position"])):
-        raise exceptions.NotFound404Exception('position with specified ID not found')
-
-    # check to make sure election exists
-    if not elections_alchemy.query_election(election_id=str(election_id)):
-        raise exceptions.NotFound404Exception('election with specified ID not found')
 
 
 def check_permissions(user):
@@ -165,7 +80,8 @@ class VoteHandler(BaseHandler):
         # validate parameters
         required_parameters = ('election', 'position', 'vote')
         validate_parameters(body_json, required_parameters)
-        validate_vote(user, body_json)
+        body_json['username'] = str(user.username)
+        elections_model.Vote.validate(body_json)
 
         # create new vote
         vote = elections_model.Vote()
@@ -220,21 +136,24 @@ class SpecificVoteHandler(BaseHandler):
         vote = elections_alchemy.query_vote(vote_id=vote_id, election=current_election.id, username=str(user.username))
         if vote == list():
             raise exceptions.NotFound404Exception('vote with specified ID not found')
+        vote = vote[0]
 
         # validate parameters
         required_parameters = ('id', 'election', 'position', 'vote', 'username')
         validate_parameters(body_json, required_parameters)
-        validate_vote(user, body_json, vote[0])
+        body_json['username'] = str(user.username)
+        elections_model.Vote.validate(body_json, vote)
 
         # update vote
         for parameter in required_parameters:
             if parameter not in ('id', 'username'):
-                setattr(vote[0], parameter, body_json[parameter])
-        setattr(vote[0], 'username', str(user.username))
-        elections_alchemy.add_or_update(vote[0])
+                setattr(vote, parameter, body_json[parameter])
+        setattr(vote, 'username', str(user.username))
+        elections_alchemy.add_or_update(vote)
 
         # response
-        self.write(vote[0].serialize())
+        self.set_status(200)
+        self.write(vote.serialize())
 
 
 class ElectionHandler(BaseHandler):
@@ -264,7 +183,7 @@ class ElectionHandler(BaseHandler):
         # validate parameters
         required_parameters = ('election_type', 'start', 'end')
         validate_parameters(body_json, required_parameters)
-        validate_election(body_json)
+        elections_model.Election.validate(body_json)
 
         # create new election
         election = elections_model.Election()
@@ -311,7 +230,7 @@ class SpecifiedElectionHandler(BaseHandler):
         # validate parameters
         required_parameters = ('id', 'election_type', 'start', 'end')
         validate_parameters(body_json, required_parameters)
-        validate_election(body_json)
+        elections_model.Election.validate(body_json)
 
         # update election
         for parameter in required_parameters:
@@ -365,7 +284,7 @@ class PositionHandler(BaseHandler):
         # validate parameters
         required_parameters = ('position', 'election_type', 'active')
         validate_parameters(body_json, required_parameters)
-        validate_position(body_json)
+        elections_model.Position.validate(body_json)
 
         # create new position
         position = elections_model.Position()
@@ -402,7 +321,7 @@ class SpecifiedPositionHandler(BaseHandler):
         # validate parameters
         required_parameters = ('id', 'position', 'election_type', 'active')
         validate_parameters(body_json, required_parameters)
-        validate_position(body_json)
+        elections_model.Position.validate(body_json)
 
         # get position
         position = elections_alchemy.query_position(position_id=str(position_id))
@@ -452,7 +371,8 @@ class CandidateHandler(BaseHandler):
         # validate parameters
         required_parameters = ('position', 'username', 'display_name')
         validate_parameters(body_json, required_parameters)
-        validate_candidate(body_json, election_id)
+        body_json['election'] = election_id
+        elections_model.Candidate.validate(body_json)
 
         # create new candidate
         candidate = elections_model.Candidate()
@@ -490,7 +410,8 @@ class SpecifiedCandidateHandler(BaseHandler):
         # validate parameters
         required_parameters = ('id', 'election', 'position', 'username', 'display_name')
         validate_parameters(body_json, required_parameters)
-        validate_candidate(body_json, election_id)
+        body_json['election'] = election_id
+        elections_model.Candidate.validate(body_json)
 
         # get election
         election = elections_alchemy.query_election(election_id=str(election_id))
