13b765093aaa82abfd361c7fec2eb43d92f6457f
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2019-03-28 12:15:34 -0800

    Moved elections to mysql.

--- aswwu/alchemy_new/elections.py
+++ aswwu/alchemy_new/elections.py
@@ -2,26 +2,24 @@
 
 import logging
 
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from aswwu.alchemy_new import connection
 import aswwu.models.bases as base
 import aswwu.models.elections as elections_model
 from datetime import datetime
 
-ElectionBase = base.ElectionBase
-
 logger = logging.getLogger("aswwu")
 
-# defines the databases URLs relative to "server.py"
-election_engine = create_engine("sqlite:///../databases/elections.db")
-# create the model tables if they don't already exist
-ElectionBase.metadata.create_all(election_engine)
+ElectionBase = base.ElectionBase
 
-# same for elections
-ElectionBase.metadata.bind = election_engine
-election_dbs = sessionmaker(bind=election_engine)
+# bind instances of the databases to corresponding variables
+ElectionBase.metadata.bind = connection
+election_dbs = sessionmaker(bind=connection)
 election_db = election_dbs()
+
+# create the model tables if they don't already exist
+ElectionBase.metadata.create_all()
 
 
 def add_or_update(thing):
