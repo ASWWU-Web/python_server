166bd63e50e18192a7c4a4f1ba3d48b4b68b7fae
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2019-03-28 12:12:12 -0800

    Moved archives to mysql.

--- aswwu/alchemy_new/archive.py
+++ aswwu/alchemy_new/archive.py
@@ -2,17 +2,17 @@
 
 import logging
 
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from aswwu.alchemy_new import connection
 from aswwu.archive_models import ArchiveBase
 
 logger = logging.getLogger("aswwu")
 
-# defines the databases URLs relative to "server.py"
-archive_engine = create_engine("sqlite:///../databases/archives.db")
-
-# same for archives
-ArchiveBase.metadata.bind = archive_engine
-archive_dbs = sessionmaker(bind=archive_engine)
+# bind instances of the databases to corresponding variables
+ArchiveBase.metadata.bind = connection
+archive_dbs = sessionmaker(bind=connection)
 archive_db = archive_dbs()
+
+# create the model tables if they don't already exist
+ArchiveBase.metadata.create_all()
