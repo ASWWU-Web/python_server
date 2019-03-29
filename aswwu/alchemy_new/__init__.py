976a329805657fc274b33b80fc44303fadc3a673
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2019-03-28 17:01:34 -0800

    Updated environemnt variable names to be compatible with AWS RDS.

--- aswwu/alchemy_new/__init__.py
+++ aswwu/alchemy_new/__init__.py
@@ -1,9 +1,9 @@
 from os import getenv
 from sqlalchemy import create_engine
 
-engine = create_engine('mysql+pymysql://{}:{}@{}:{}/{}'.format(getenv('DB_USER'),
-                                                               getenv('DB_PASSWORD'),
-                                                               getenv('DB_IP', '127.0.0.1'),
-                                                               getenv('DB_PORT', '3306'),
-                                                               getenv('DB_NAME')))
+engine = create_engine('mysql+pymysql://{}:{}@{}:{}/{}'.format(getenv('RDS_USERNAME'),
+                                                               getenv('RDS_PASSWORD'),
+                                                               getenv('RDS_HOSTNAME', '127.0.0.1'),
+                                                               getenv('RDS_PORT', '3306'),
+                                                               getenv('RDS_DB_NAME')))
 connection = engine.connect()
