ccd96cf3ede3ccc5abd78b405bc8768b6228b20c
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2019-04-15 15:21:22 -0700

    Added new featured field to jobforms table. Updated jobform serializer.

--- aswwu/models/forms.py
+++ aswwu/models/forms.py
@@ -15,19 +15,35 @@ class JobForm(JobsBase):
     owner = Column(String(100), nullable=False)
     questions = relationship("JobQuestion", backref="jobforms", lazy="joined")
     image = Column(String(100), nullable=False)
+    featured = Column(Boolean, default=False)
 
     def serialize(self):
         questions = []
         for question in self.questions:
             questions.append(question.serialize())
-        return {'job_name': self.job_name, 'job_description': self.job_description,
-                'visibility': self.visibility, 'owner': self.owner, 'department': self.department,
-                'image': self.image, 'questions': questions, 'jobID': self.id}
+        return {
+            'job_name': self.job_name,
+            'job_description': self.job_description,
+            'visibility': self.visibility,
+            'owner': self.owner,
+            'department': self.department,
+            'image': self.image,
+            'questions': questions,
+            'jobID': self.id,
+            'featured': self.featured
+        }
 
     def min(self):
-        return {'job_name': self.job_name, 'job_description': self.job_description,
-                'department': self.department, 'image': self.image, 'jobID': self.id,
-                'visibility': self.visibility, 'owner': self.owner}
+        return {
+            'job_name': self.job_name,
+            'job_description': self.job_description,
+            'department': self.department,
+            'image': self.image,
+            'jobID': self.id,
+            'visibility': self.visibility,
+            'owner': self.owner,
+            'featured': self.featured
+        }
 
 
 class JobQuestion(JobsBase):
