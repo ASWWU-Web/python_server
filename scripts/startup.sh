48af3c0114bbf3f3988e6c4b0925455802e87af3
Stephen Ermshar <stephen.ermshar@gmail.com>
2020-03-30 16:48:48 -0700

    revert startup

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,4 +1,4 @@
-sudo screen -dSm ps1 python server.py --port=8881
-sudo screen -dSm ps2 python server.py --port=8882
-sudo screen -dSm ps3 python server.py --port=8883
-sudo screen -dSm ps4 python server.py --port=8884
+pipenv run sudo screen -dSm ps1 python server.py --port=8881
+pipenv run sudo screen -dSm ps2 python server.py --port=8882
+pipenv run sudo screen -dSm ps3 python server.py --port=8883
+pipenv run sudo screen -dSm ps4 python server.py --port=8884
