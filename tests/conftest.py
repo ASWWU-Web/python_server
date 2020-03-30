81af08c16e899645205d2b575d86d18f31fabf07
Stephen Ermshar <stephen.ermshar@gmail.com>
2020-03-30 16:41:21 -0700

    remove reference to default.conf

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,12 +5,12 @@ from tornado.options import options, define
 import threading
 
 from src.aswwu.application import Application, start_server, stop_server
+from settings import testing
 
-conf_file = "tests/default.conf"
-define("port", type=int)
-define("log_name")
-define("current_year")
-tornado.options.parse_config_file(conf_file)
+define("port", default=testing['port'], type=int)
+define("log_name", default=testing['log_name'])
+define("current_year", default=testing['current_year'])
+tornado.options.parse_command_line()
 options_defined = True
 
 
