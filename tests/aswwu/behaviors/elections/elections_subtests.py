6756bb5a40c8ee128510acdd71734b80dd461c8d
kadensu <ksukachevin@gmail.com>
2020-04-03 18:58:25 -0400

    get request test for elections/election

--- tests/aswwu/behaviors/elections/elections_subtests.py
+++ tests/aswwu/behaviors/elections/elections_subtests.py
@@ -3,21 +3,29 @@ import tests.aswwu.behaviors.elections.elections_requests as elections_requests
 from tests.utils import load_csv
 from tests.aswwu.data.paths import ELECTIONS_PATH
 
+
 def send_get_election():
     resp = elections_requests.get_election()
     assert (resp.status_code == 200)
+    resp_text = json.loads(resp.text)
+    elections_csv = load_csv(ELECTIONS_PATH)
+
+    for i in range(len(resp_text['elections'])):
+        assert (resp_text['elections'][i]['election_type'] == elections_csv[i]['election_type'])
+        assert (resp_text['elections'][i]['name'] == elections_csv[i]['name'])
+        assert (int(resp_text['elections'][i]['max_votes']) == int(elections_csv[i]['max_votes']))
+        assert (resp_text['elections'][i]['start'] == elections_csv[i]['start'])
+        assert (resp_text['elections'][i]['end'] == elections_csv[i]['end'])
+        assert (resp_text['elections'][i]['show_results'] == elections_csv[i]['show_results'])
 
 
 def send_post_election():
-    # DEFAULT_STATUS = 'Student'
-    # DEFAULT_ROLES = 'None'
     elections = load_csv(ELECTIONS_PATH)
     for election in elections:
         resp = elections_requests.post_election(election['election_type'], election['name'], election['max_votes'],
                                                 election['start'], election['end'], election['show_results'])
         resp_text = json.loads(resp.text)
         assert (resp.status_code == 201)
-        # assert (resp_text['token'].split('|')[0] == user['wwuid'])
         assert (resp_text['election_type'] == election['election_type'])
         assert (resp_text['name'] == election['name'])
         assert (int(resp_text['max_votes']) == int(election['max_votes']))
--- tests/aswwu/behaviors/elections/elections_tests.py
+++ tests/aswwu/behaviors/elections/elections_tests.py
@@ -5,5 +5,6 @@ from tests.conftest import testing_server
 def test_post_election(testing_server):
     send_post_election()
 
+
 def test_get_election(testing_server):
     send_get_election()
