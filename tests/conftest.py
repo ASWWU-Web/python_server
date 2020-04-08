94586fa14db1a942a6898f03e25ecbfcf386bced
Stephen Ermshar <stephen.ermshar@gmail.com>
2020-04-08 15:44:04 -0700

    use file copying to reset databases, instead of sql

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,21 +4,22 @@ import tornado.ioloop
 from tornado.options import options, define
 import threading
 
+from src.aswwu.application import start_server, stop_server
+from utils import replace_databases
 from settings import database, testing
-database['location'] = testing['database']
 
-from src.aswwu.application import Application, start_server, stop_server
-from utils import reset_databases
 
 define("port", default=testing['port'], type=int)
 define("log_name", default=testing['log_name'])
 define("current_year", default=testing['current_year'])
 
+temp_databases_path = testing['database'] + '/temp_dbs'
+database['location'] = temp_databases_path
+testing['dev'] = False
 
 @pytest.fixture()
 def testing_server():
+    replace_databases(testing['database'], temp_databases_path)
     server, event_loop_thread = start_server()
-    reset_databases()
     yield
-    reset_databases()
     stop_server(server, event_loop_thread)
