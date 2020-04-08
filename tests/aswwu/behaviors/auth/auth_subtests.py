bc42e098ccdfad26aeedf72bf105ab60af5e1e55
Stephen Ermshar <stephen.ermshar@gmail.com>
2020-04-08 15:44:44 -0700

    refactor auth subtests

--- tests/aswwu/behaviors/auth/auth_subtests.py
+++ tests/aswwu/behaviors/auth/auth_subtests.py
@@ -2,17 +2,42 @@ import tests.aswwu.behaviors.auth.auth_requests as auth_requests
 import tests.utils as utils
 from tests.aswwu.data.paths import USERS_PATH
 import json
+from requests import Session, cookies
 
 DEFAULT_STATUS = 'Student'
 DEFAULT_ROLES = ['None', '']
 
 
+def assert_verify_login(user):
+    """
+    Use this function to make sure a test user is logged in on the server.
+    Tests whether a user that authenticated with SAML can use the verify endpoint.
+    :param user: a user object loaded from a csv file
+    :return: the text of the get response verifying this user
+    """
+    post_response = auth_requests.post_verify(user['wwuid'], user['full_name'], user['email'])
+    assert_verify_response(post_response, user)
+    auth_token = json.loads(post_response.text)['token']
+    auth_cookie = cookies.create_cookie('token', auth_token)
+    session = Session()
+    session.cookies.set_cookie(auth_cookie)
+    get_response = auth_requests.get_verify(session)
+    assert_verify_response(get_response, user)
+    return json.loads(get_response.text)
+
+
 def assert_verify_response(response, user):
+    """
+    Tests whether data returned from the verify endpoint matches the provided user.
+    :param response: the response from the verify endpoint
+    :param user: a user object loaded form a csv file
+    :return: None
+    """
     response_text = json.loads(response.text)
     assert(response.status_code == 200)
-    assert (response_text['token'].split('|')[0] == user['wwuid'])
     assert (response_text['user']['username'] == user['username'])
     assert (response_text['user']['wwuid'] == user['wwuid'])
     assert (response_text['user']['roles'] in DEFAULT_ROLES)
     assert (response_text['user']['status'] == DEFAULT_STATUS)
     assert (response_text['user']['full_name'] == user['full_name'])
+    assert (response_text['token'].split('|')[0] == user['wwuid'])
--- tests/aswwu/behaviors/auth/test_auth.py
+++ tests/aswwu/behaviors/auth/test_auth.py
@@ -1,6 +1,6 @@
 import tests.aswwu.behaviors.auth.auth_requests as auth_requests
 import requests
-from tests.aswwu.behaviors.auth.auth_subtests import assert_verify_response
+from tests.aswwu.behaviors.auth.auth_subtests import assert_verify_response, assert_verify_login
 from tests.conftest import testing_server
 from tests.utils import load_csv
 from tests.aswwu.data.paths import USERS_PATH
@@ -17,11 +17,4 @@ def test_post_verify(testing_server):
 def test_get_verify(testing_server):
     users = load_csv(USERS_PATH)
     for user in users:
-        response = auth_requests.post_verify(user['wwuid'], user['full_name'], user['email'])
-        assert_verify_response(response, user)
-        token = json.loads(response.text)['token']
-        cookie = requests.cookies.create_cookie('token', token)
-        session = requests.Session()
-        session.cookies.set_cookie(cookie)
-        response = auth_requests.get_verify(session)
-        assert_verify_response(response, user)
+        assert_verify_login(user)
