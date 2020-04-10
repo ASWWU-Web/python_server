2ad469a2ed8fb1ea019c889bca321eecbd2e0cc8
Stephen Ermshar <stephen.ermshar@gmail.com>
2020-04-10 16:28:11 -0700

    change imports

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,20 @@
 import time
 import pytest
-import tornado.ioloop
-from tornado.options import options, define
+import tornado.options
 import threading
 
 from utils import replace_databases
 from settings import database, testing
 
 
-define("port", default=testing['port'], type=int)
-define("log_name", default=testing['log_name'])
-define("current_year", default=testing['current_year'])
+tornado.options.define("port", default=testing['port'], type=int)
+tornado.options.define("log_name", default=testing['log_name'])
+tornado.options.define("current_year", default=testing['current_year'])
 
 temp_databases_path = testing['database'] + '/temp_dbs'
 database['location'] = temp_databases_path
 testing['dev'] = False
+
 
 @pytest.fixture()
 def testing_server():
@@ -23,6 +23,6 @@ def testing_server():
     # application must be imported after databases are setup
     from src.aswwu.application import start_server, stop_server
 
-    server, event_loop_thread = start_server()
+    server, event_loop_thread, ioloop = start_server()
     yield
-    stop_server(server, event_loop_thread)
+    stop_server(server, event_loop_thread, ioloop)
