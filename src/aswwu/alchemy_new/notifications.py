a743b5affb19dc33867f9b789c22e90b28af728e
Michael Stacy <michael.stacy@wallawalla.edu>
2020-04-13 23:12:36 -0700

    added more to the alchemy code so it should be able to get and post. Need more help on something before i can test this

--- src/aswwu/alchemy_new/notifications.py
+++ src/aswwu/alchemy_new/notifications.py
@@ -20,12 +20,12 @@ NotificationsBase.metadata.create_all(notifications_engine)
 
 # bind instances of the databases to corresponding variables
 NotificationsBase.metadata.bind = notifications_engine
-notifications_dbs = sessionmaker(bind=jobs_engine)
+notifications_dbs = sessionmaker(bind=notifications_engine)
 notifications_db = notifications_dbs()
 
 
 # updates a model, or creates it if it doesnt exist
-def add_or_update_notification(thing):
+def add_or_update(thing):
     try:
         notifications_db.add(thing)
         notifications_db.commit()
@@ -34,33 +34,38 @@ def add_or_update_notification(thing):
         logger.info(e)
         notifications_db.rollback()
 
-# TODO decide how to query specific notifications
+def query_notifications(notification_text=None, notification_links=None, start_time=None, end_time=None,
+                   severity=None, visible=None):
+    """
+    Queries the database for notifications matching the specified parameters.
+    :param
+    :param
+    :param
+    :param
+    :param
+    :param
+    :param
+    :param
+    :return: Returns a list of all matched objects in the query.
+    """
+    thing = None
+    try:
+        thing = notifications_db.query(notifications_model.Notification)
+        if notification_text is not None:
+            thing = thing.filter_by(text=str(text))
+        if notification_links is not None:
+            thing = thing.filter_by(links=str(links))
+        if start_time is not None:
+            thing = thing.filter_by(notifications_model.Notification.start <= start_before)
+        if end_time is not None:
+            thing = thing.filter_by(notifications_model.Notification.start >= start_before)
+        if severity is not None:
+            thing = thing.filter(severity=int(severity))
+        if visible is not None:
+            thing = thing.filter(visible=int(visible))
+        thing = thing.all()
+    except Exception as e:
+        logger.info(e)
+        notifications_db.rollback()
+    return thing
 
-#def query_by_notification_(model, name):
-#    thing = None
-#    try:
-#        thing = jobs_db.query(model).options(joinedload('*')).filter_by(job_name=str(name)).all()
-#    except Exception as e:
-#        logger.info(e)
-#        jobs_db.rollback()
-#    return thing
-#
-#
-#def query_all_forms(model):
-#    thing = None
-#    try:
-#        thing = jobs_db.query(model).all()
-#    except Exception as e:
-#        logger.info(e)
-#        jobs_db.rollback()
-#    return thing
-#
-#
-## permanently deletes a given model
-#def delete_thing_forms(thing):
-#    try:
-#        jobs_db.delete(thing)
-#        jobs_db.commit()
-#    except Exception as e:
-#        logger.info(e)
-#        jobs_db.rollback()
