768c414b7f37c86a100e7c4237a1e88323605ef9
Michael Stacy <michael.stacy@wallawalla.edu>
2020-04-15 17:08:17 -0700

    changed to use the correct base and updated var names

--- src/aswwu/models/notifications.py
+++ src/aswwu/models/notifications.py
@@ -6,7 +6,7 @@ from src.aswwu.models.bases import NotificationsBase
 
 #size is arbitrary for now until we decide on it
 
-class Notification(ElectionBase):
+class Notification(NotificationsBase):
     notification_text = Column(String(500))
     notification_links = Column(String(500))
     start_time = Column(DateTime)
@@ -17,8 +17,8 @@ class Notification(ElectionBase):
     def serialize(self):
         # determine if show_results field should be cast to a string
         return {
-            'notifications_text': self.text,
-            'notification_links': self.links,
+            'notification_text': self.notification_text,
+            'notification_links': self.notification_links,
             'start_time': datetime.strftime(self.start_time, '%Y-%m-%d %H:%M:%S.%f'),
             'end_time': datetime.strftime(self.end_time, '%Y-%m-%d %H:%M:%S.%f'),
             'severity': self.visible,
