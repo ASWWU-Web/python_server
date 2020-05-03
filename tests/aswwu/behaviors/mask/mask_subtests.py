fa5a2b58d58eaf78cbbba43ebc9953aeeee2e29b
Stephen Ermshar <10591356+stephenermshar@users.noreply.github.com>
2020-05-02 19:54:58 -0700

    refactor mask tests file structure

--- tests/aswwu/behaviors/mask/mask_subtests.py
+++
@@ -1,5 +0,0 @@
-import tests.aswwu.behaviors.mask.mask_requests as mask_requests
-import tests.utils as utils
-from tests.aswwu.data.paths import USERS_PATH
-import json
-from requests import Session, cookies
