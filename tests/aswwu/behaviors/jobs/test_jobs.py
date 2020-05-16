6f132a347dd612bd4efdb9b76076cd023b9abdaf
Stephen Ermshar <10591356+stephenermshar@users.noreply.github.com>
2020-05-16 12:24:14 -0700

    add test functions

--- tests/aswwu/behaviors/jobs/test_jobs.py
+++ tests/aswwu/behaviors/jobs/test_jobs.py
@@ -10,3 +10,53 @@ from settings import environment
 import settings
 
 CURRENT_YEAR = settings.environment["current_year"]
+
+
+# "new": "job/new",
+def test_new_job():
+    pass
+
+
+# "job_view": "job/view",
+def test_job_view():
+    pass
+
+
+# "job_delete": "job/delete",
+def test_job_delete():
+    pass
+
+
+# "job_edit": "job/edit",
+def test_job_edit():
+    pass
+
+
+# "app_submit": "application/submit",
+def test_app_submit():
+    pass
+
+
+# "app_view": "application/view",
+def test_app_view():
+    pass
+
+
+# "app_status": "application/status",
+def test_app_view():
+    pass
+
+
+# "app_export": "application/export",
+def test_app_export():
+    pass
+
+
+# "resume_upload": "resume/upload",
+def test_resume_upload():
+    pass
+
+
+# "resume_download": "resume/download",
+def test_resume_download():
+    pass
