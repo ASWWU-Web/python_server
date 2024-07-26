4ca002e5d8943bff346a35253b5bca7768450f35
Riley Smith <github@rileyy.dev>
2024-07-25 23:32:51 -0700

    fix the validation issues

--- tests/aswwu/behaviors/auth/auth_subtests.py
+++ tests/aswwu/behaviors/auth/auth_subtests.py
@@ -1,3 +1,4 @@
+import base64
 import tests.aswwu.behaviors.auth.auth_requests as auth_requests
 import tests.utils as utils
 from tests.aswwu.data.paths import USERS_PATH
@@ -35,11 +36,12 @@ def assert_verify_response(response, user):
     :return: None
     """
     response_text = json.loads(response.text)
+    print(response_text)
     assert (response.status_code == 200)
     assert (response_text['user']['username'] == user['username'])
     assert (response_text['user']['wwuid'] == user['wwuid'])
     assert (response_text['user']['roles'] in DEFAULT_ROLES)
     assert (response_text['user']['status'] == DEFAULT_STATUS)
     assert (response_text['user']['full_name'] == user['full_name'])
-    assert (response_text['token'].split('|')[0] == user['wwuid'])
+    assert (base64.b64decode(response_text['token']).decode('ascii').split('|')[0] == user['wwuid'])
 
